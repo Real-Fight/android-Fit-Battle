32ac1207d2499626d2725bcd66362414c59d6353
qpeterp <lsepeter0827@naver.com>
2024-10-31 21:01:12 +0900

    Fix :: NavHost material3Api optIn

--- app/src/main/java/com/qpeterp/fitbattle/presentation/root/main/view/AppActivity.kt
+++ app/src/main/java/com/qpeterp/fitbattle/presentation/root/main/view/AppActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -19,6 +20,7 @@ class AppActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MyApp() {
         val navController = rememberNavController()
