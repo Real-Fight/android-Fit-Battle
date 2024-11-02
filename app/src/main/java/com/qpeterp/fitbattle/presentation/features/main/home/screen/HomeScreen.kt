ab84e62478c25bb032f9d7650387a07a11e712ae
qpeterp <lsepeter0827@naver.com>
2024-11-02 15:44:27 +0900

    Feat :: ChallengeCard ui

--- app/src/main/java/com/qpeterp/fitbattle/presentation/features/main/home/screen/HomeScreen.kt
+++ app/src/main/java/com/qpeterp/fitbattle/presentation/features/main/home/screen/HomeScreen.kt
@@ -1,26 +1,36 @@
 package com.qpeterp.fitbattle.presentation.features.main.home.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -39,14 +49,14 @@ fun HomeScreen(
     navController: NavController,
     viewModel: HomeViewModel = viewModel()
 ) {
-    val scrollState = rememberScrollState()
+    val screenScrollState = rememberScrollState()
+    val challengeScrollState = rememberScrollState()
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .scrollable(
-                state = scrollState,
-                orientation = Orientation.Vertical
-            )
+            .verticalScroll(
+                state = screenScrollState,
+            ),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         Box(
             modifier = Modifier
@@ -105,6 +115,34 @@ fun HomeScreen(
                 .padding(horizontal = 20.dp, vertical = 16.dp)
         )
 
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .horizontalScroll(
+                    state = challengeScrollState,
+                )
+                .padding(horizontal = 20.dp)
+        ) {
+            ChallengeCard(
+                mainColor = Colors.LightPrimaryColor,
+                subColor = Colors.LightPrimaryColorDark,
+                title = "전신 7X4\n챌린지",
+                content = "온 몸의 근육들을 파괴해보세요!",
+                animation = "", // TODO: 애니메이션 추가 얘정
+            ) {
+
+            }
+            ChallengeCard(
+                mainColor = Colors.DarkPrimaryColor,
+                subColor = Colors.DarkPrimaryColorDark,
+                title = "문가인 챌린지",
+                content = "문가인을 뛰어넘어 보세요!",
+                animation = "" // TODO: 애니메이션 추가 얘정
+            ) {
+
+            }
+        }
+
         Text(
             text = "훈련",
             fontSize = 20.sp,
@@ -112,7 +150,8 @@ fun HomeScreen(
             color = Colors.Black,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 20.dp, vertical = 16.dp)
+                .padding(horizontal = 20.dp)
+                .padding(bottom = 16.dp)
         )
 
         Text(
@@ -122,7 +161,8 @@ fun HomeScreen(
             color = Colors.Black,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 24.dp, vertical = 16.dp)
+                .padding(horizontal = 24.dp)
+                .padding(bottom = 16.dp)
         )
 
         TrainingCard(
@@ -179,6 +219,73 @@ private fun TodayMissionButton(
                 .size(28.dp)
                 .align(Alignment.Center)
         )
+    }
+}
+
+@Composable
+private fun ChallengeCard(
+    mainColor: Color,
+    subColor: Color,
+    title: String,
+    content: String,
+    textColor: Color = Colors.White,
+    buttonColor: Color = Colors.White,
+    animation: String, // 추후, 업데이트 예정
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .width(320.dp)
+            .height(240.dp)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(mainColor, subColor),
+                    start = Offset(0f, 0f),
+                    end = Offset.Infinite // 끝 지점을 설정 (예: 무한 방향으로 확장)
+                ), RoundedCornerShape(12.dp)
+            )
+            .padding(20.dp)
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = textColor,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+
+        Column (
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = content,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = textColor,
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Button(
+                onClick = { onClick() },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(100.dp), // 모서리를 둥글게 설정
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = buttonColor,
+                    containerColor = buttonColor
+                )
+            ) {
+                Text(
+                    text = "시작",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = mainColor
+                )
+            }
+        }
     }
 }
 
