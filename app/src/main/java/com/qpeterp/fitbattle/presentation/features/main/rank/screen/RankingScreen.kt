6b95ae64ec996c3513d92ae619c7e58b7cd96f95
qpeterp <lsepeter0827@naver.com>
2024-11-06 17:51:36 +0900

    Feat :: RankingScreen rankList feature

--- app/src/main/java/com/qpeterp/fitbattle/presentation/features/main/rank/screen/RankingScreen.kt
+++ app/src/main/java/com/qpeterp/fitbattle/presentation/features/main/rank/screen/RankingScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,7 +15,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
