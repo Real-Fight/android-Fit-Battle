acc1a8257dbde218c263e8318c106091008b9531
qpeterp <lsepeter0827@naver.com>
2024-11-20 17:41:43 +0900

    Fix :: ShimmerEffect animationSpec 1000 -> 3000

--- app/src/main/java/com/qpeterp/fitbattle/presentation/extensions/shimmerEffect.kt
+++ app/src/main/java/com/qpeterp/fitbattle/presentation/extensions/shimmerEffect.kt
@@ -26,7 +26,7 @@ fun Modifier.shimmerEffect(radius: Dp): Modifier = composed {
         initialValue = -2 * size.width.toFloat(),
         targetValue = 2 * size.width.toFloat(),
         animationSpec = infiniteRepeatable(
-            animation = tween(1000)
+            animation = tween(3000)
         ),
         label = ""
     )
